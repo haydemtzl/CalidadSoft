42b5fa2c590b9ade7c3716cb888f6f51530917f0
Jaycorpstudios <jaycorpstudios@me.com>
2014-01-21 13:02:15 -0600

    modificada la configuracion y agregado el index

--- config.php
+++ config.php
@@ -4,31 +4,31 @@
  */
 
 /** Nombre de la base de datos */
-define('DB_NAME', 'vivelatina');
+define('DB_NAME', 'newsite');
 
 /** Usuario de la base de datos */
 define('DB_USER', 'root');
 
 /** Password de la base de datos */
-define('DB_PASSWORD', 'c83af90KAl1');
+define('DB_PASSWORD', 'c83af91KAl1');
 
 /** Host (si se separa la bd del apache) */
 define('DB_HOST', 'localhost');
 
 /** Carpeta base del sistema (para imports, includes y demás) */
-define('FILES_BASE', '/Applications/XAMPP/xamppfiles/htdocs/vivelatina/webpanel/');
+define('FILES_BASE', '/Applications/XAMPP/xamppfiles/htdocs/newsite/webpanel/');
 
 /** Carpeta base del sistema de la parte publica (para imports, includes y demás) */
-define('FILES_BASE_SITE', '/Applications/XAMPP/xamppfiles/htdocs/vivelatina/site/');
+define('FILES_BASE_SITE', '/Applications/XAMPP/xamppfiles/htdocs/newsite/site/');
 
 /** URL Base del webpanel */
-define('URL_BASE','http://localhost/vivelatina/webpanel/');
+define('URL_BASE','http://localhost/newsite/webpanel/');
 
 /** URL Base del sitio */
-define('URL_BASE_SITE','http://localhost/vivelatina/site/');
+define('URL_BASE_SITE','http://localhost/newsite/site/');
 
 /** @name SESSION_ID Nombre de la variable principal de sesión */
-define('SESSION_ID', 'editor');
+define('SESSION_ID', 'newsiteid_secret');
 
 /** @name LOG_DIR Carpeta para almacenar los logs */
 define('LOG_DIR','logs/');
